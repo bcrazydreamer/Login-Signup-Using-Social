399bef21f43306bd1bb393793637c60fc477eaae
bcrazydreamer <31788510+bcrazydreamer@users.noreply.github.com>
2018-06-11 14:00:35 +0530

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -1,13 +1,13 @@
 module.exports = {
 	'Facebook' : {
-		'clientID': '804036696474128',
-		'clientSecret': 'ee87b56dfa4c294dd90156631b312c13',
+		'clientID': 'xxxxxxxxxxxxxxxxxxxxxxxx',
+		'clientSecret': 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
 		'callbackURL': 'https://cqlogin.herokuapp.com/auth/facebook/callback'
 	},
 
 	'Google' : {
-		'clientID': '310496275332-vd9ddbdr1f8vko41p7g5vgkqoci5ovhv.apps.googleusercontent.com',
-		'clientSecret': 'zJ5B1rJkJ3By4h8vb6M--tef',
+		'clientID': 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+		'clientSecret': 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
 		'callbackURL': 'https://cqlogin.herokuapp.com/auth/google/callback'
 	}
 }
